9ad23c08c51f23bc969f2674b136bbaf6dd0edd7
Prashant Kumar Verma <prashant.kumar.verma@nl.abnamro.com>
2018-02-04 21:01:53 +0100

    added

---
+++ cSharpLearning-AsyncAwait/Demo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Main thread started");
+            Thread thread = new Thread(new ThreadStart(Iteration));
+            thread.Start();
+            Console.WriteLine("Main Thread ends");
+        }
+
+        private static void Iteration()
+        {
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(1000);
+            }
+        }
+
+
+    }
+}
